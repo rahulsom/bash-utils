82493f73fee7a4fd0d30a99ef3dca9a18a70f9de
rahul somasunderam <rahul@rahul-dv6000.(none)>
2011-05-03 13:17:12 -0700

    TO add colors

--- src/colors.sh
+++ src/colors.sh
@@ -1,42 +1,34 @@
 #!/bin/bash
 #
 
+ANESC="\033["
+ANREG="00"
+ANBOL="01"
+ANDIS="02"
+ANUND="04"
+ANSTR="09"
+
+ANBG="4"
+ANFG="3"
+
+ANK="0"
+ANR="1"
+ANG="2"
+ANY="3"
+ANB="4"
+ANM="5"
+ANC="6"
+ANW="7"
+ANDEF="8"
+
+ANEND="m"
+
 # Reset
-ansi_reset ()     { echo -e -n "\033[00;0m"; }
+ANRESET="${ANESC}${ANREG};${ANBG}${ANDEF};${ANFG}${ANDEF}${ANEND}"
 
-# Effects
-ansi_bold ()      { echo -e -n "\033[00;1m"; }
-ansi_italics ()   { echo -e -n "\033[00;3m"; }
-ansi_underline () { echo -e -n "\033[00;4m"; }
-ansi_inverse ()   { echo -e -n "\033[00;7m"; }
-ansi_strike ()    { echo -e -n "\033[00;9m"; }
-
-# Reverse effects
-ansi_nbold ()      { echo -e -n "\033[00;22m"; }
-ansi_nitalics ()   { echo -e -n "\033[00;23m"; }
-ansi_nunderline () { echo -e -n "\033[00;24m"; }
-ansi_ninverse ()   { echo -e -n "\033[00;27m"; }
-ansi_nstrike ()    { echo -e -n "\033[00;29m"; }
-
-# Foreground
-ansi_fg_black()   { echo -e -n "\033[00;30m"; }
-ansi_fg_red()     { echo -e -n "\033[00;31m"; }
-ansi_fg_green()   { echo -e -n "\033[00;32m"; }
-ansi_fg_yellow()  { echo -e -n "\033[00;33m"; }
-ansi_fg_blue()    { echo -e -n "\033[00;34m"; }
-ansi_fg_magenta() { echo -e -n "\033[00;35m"; }
-ansi_fg_cyan()    { echo -e -n "\033[00;35m"; }
-ansi_fg_white()   { echo -e -n "\033[00;37m"; }
-ansi_fg_default() { echo -e -n "\033[00;39m"; }
-
-# Background
-ansi_bg_black()   { echo -e -n "\033[00;40m"; }
-ansi_bg_red()     { echo -e -n "\033[00;41m"; }
-ansi_bg_green()   { echo -e -n "\033[00;42m"; }
-ansi_bg_yellow()  { echo -e -n "\033[00;43m"; }
-ansi_bg_blue()    { echo -e -n "\033[00;44m"; }
-ansi_bg_magenta() { echo -e -n "\033[00;45m"; }
-ansi_bg_cyan()    { echo -e -n "\033[00;45m"; }
-ansi_bg_white()   { echo -e -n "\033[00;47m"; }
-ansi_bg_default() { echo -e -n "\033[00;49m"; }
-
+#
+# Test case
+#
+if [ $(basename $0) = colors.sh ]; then
+  echo -e "${ANESC}${ANSTR};${ANBOL};${ANBG}${ANG}${ANEND}Something${ANRESET}"
+fi
